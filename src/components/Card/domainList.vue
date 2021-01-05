915939b5999c8271262743bc869deba00ca3d001
Lucas Augusto <lucas.augusto5061@gmail.com>
2021-01-05 17:45:52 -0300

    add css for show domains in grid

--- src/components/Card/domainList.vue
+++ src/components/Card/domainList.vue
@@ -26,7 +26,7 @@
       <span class="badge badge-info">{{ domains.length }}</span>
     </h5>
 
-    <div class="card">
+    <div class="card domains">
       <div class="card-body">
         <ul class="list-group">
           <li class="list-group-item" v-for="domain in domains" v-bind:key="domain.name">
@@ -75,6 +75,7 @@ export default {
   computed: {
     domains() {
       const domains = [];
+
       for (const prefix of this.prefixes) {
         for (const sufix of this.sufixes) {
           const name = prefix + sufix;
@@ -93,4 +94,20 @@ export default {
 </script>
 
 <style scoped>
+  .domains .list-group {
+    flex-direction: row;
+    flex-wrap: wrap;
+  }
+  .domains .list-group-item {
+    margin-left: 1rem;
+    margin-bottom: 0.5rem;
+    width:  calc(33% - 1rem);
+  }
+  .domains .list-groupo-item:nth-child(3n + 1) {
+    margin-left: 0;
+  }
+  .domains .list-group-item + .list-group-item {
+    border-radius: .3rem;
+    border-top-width: 1px;
+  }
 </style>
