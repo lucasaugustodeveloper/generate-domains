405dd09cdbc8ae90055130c384cc74bb5bf91cf7
Lucas Augusto <lucas.augusto5061@gmail.com>
2021-06-28 09:38:00 -0300

    update component and vue template compiler

--- src/components/Card/domainList.vue
+++ src/components/Card/domainList.vue
@@ -29,11 +29,19 @@
     <div class="card domains">
       <div class="card-body">
         <ul class="list-group">
-          <li class="list-group-item" v-for="domain in domains" v-bind:key="domain.name">
+          <li
+            class="list-group-item"
+            v-for="domain in domains"
+            v-bind:key="domain.name"
+          >
             <div class="row">
               <div class="col-md">{{ domain.name }}</div>
               <div class="col-md text-right">
-                <a v-bind:href="domain.url" target="_blank" class="btn btn-info">
+                <a
+                  v-bind:href="domain.url"
+                  target="_blank"
+                  class="btn btn-info"
+                >
                   <span class="fa fa-shopping-cart"></span>
                 </a>
               </div>
@@ -50,12 +58,12 @@ import AppItemList from "./AppItemList";
 export default {
   name: "DomainsList",
   components: {
-    AppItemList
+    AppItemList,
   },
   data() {
     return {
       prefixes: ["Air", "Jet", "Flight", "Pod"],
-      sufixes: ["Hub", "Station", "Mart", "Try"]
+      sufixes: ["Hub", "Station", "Mart", "Try"],
     };
   },
   methods: {
@@ -70,7 +78,7 @@ export default {
     },
     deleteSufix(sufix) {
       this.sufixes.splice(this.sufixes.indexOf(sufix), 1);
-    }
+    },
   },
   computed: {
     domains() {
@@ -79,35 +87,35 @@ export default {
       for (const prefix of this.prefixes) {
         for (const sufix of this.sufixes) {
           const name = prefix + sufix;
-          const url = `https://checkout.hostgator.com.br/?a=add&sld=${name.toLowerCase()}&tld=.com.br&_ga=2.211999184.539753684.1570560389-2051749093.1570560389`;
+          const url = `https://checkout.hostgator.com.br/?a=add&sld=${name.toLowerCase()}&tld=.com.br`;
           domains.push({
             name,
-            url
+            url,
           });
         }
       }
 
       return domains;
-    }
-  }
+    },
+  },
 };
 </script>
 
 <style scoped>
-  .domains .list-group {
-    flex-direction: row;
-    flex-wrap: wrap;
-  }
-  .domains .list-group-item {
-    margin-left: 1rem;
-    margin-bottom: 0.5rem;
-    width:  calc(33% - 1rem);
-  }
-  .domains .list-groupo-item:nth-child(3n + 1) {
-    margin-left: 0;
-  }
-  .domains .list-group-item + .list-group-item {
-    border-radius: .3rem;
-    border-top-width: 1px;
-  }
+.domains .list-group {
+  flex-direction: row;
+  flex-wrap: wrap;
+}
+.domains .list-group-item {
+  margin-left: 1rem;
+  margin-bottom: 0.5rem;
+  width: calc(33% - 1rem);
+}
+.domains .list-groupo-item:nth-child(3n + 1) {
+  margin-left: 0;
+}
+.domains .list-group-item + .list-group-item {
+  border-radius: 0.3rem;
+  border-top-width: 1px;
+}
 </style>
